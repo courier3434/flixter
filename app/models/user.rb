ba1c2bb124bdcf1cf060ab802abd6ef2a0e711f6
Caitlin LaCour <lacourcaitlin@gmail.com>
2019-11-27 15:59:32 +0000

    enrollment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   has_many :courses
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
+
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def enrolled_in?(course)
+    enrolled_courses = enrollments.collect(&:course)
+        enrollment.course
+    end
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
+
+
 end
